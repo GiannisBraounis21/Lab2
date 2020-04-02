e7d35260d866501627a129c2d1210eaaf9a2b8bf
Γιαννης Μπραουνης <giannisbraounis@gmail.com>
2020-04-02 22:52:06 +0300

    Excersice One

--- Lab2E/Source.asm
+++ Lab2E/Source.asm
@@ -16,6 +16,44 @@ INCLUDE Irvine32.inc
 	z SWORD -30d
 .code
 main PROC
+	;Askisi 1
+	mov edx , OFFSET mCalculating ;Load EDX with the mCalculating string OFFSET
+	call WriteString ;Show the message on screen
+
+	;Calculating address of array1(2,2)
+	;The result will be 32bit hense the arithmetic operations
+	;are performed on 32bit registers
+
+	;Move row to EBX register
+	movzx ebx,x ;Converts 8bit variable to 32bit.The rest binary numbers are filled with zeroes
+	shl ebx,2 ;EBX=row*4 (bitwise shift operation 2 times)
+
+	;Move row to ECX register
+	movzx ecx,y ;Converts 8bit variable to 32bit.The rest binary numbers are filled with zeroes
+	add ebx,ecx ;EBX=row * 4 + column
+	shl ebx,2   ;EBX=(row * 4 + column) * 4
+	
+	mov eax,[array1+ebx] ;mov designated array element to ECX using INDIRECT ADDRESSING
+	;EAX is used later for the command WriteInt
+	mov edx , OFFSET mAccessElement ;Load EDX with the mAccessElement string OFFSET
+	call WriteString ;Show the message on the screen
+	call WriteInt ;Show the element value on the screen
+	call Crlf ;Go to next line
+
+	movsx ecx,z ;move the value of z into ECX
+	add eax,ecx	;EAX = 22 + ECX = -8
+	mov edx,OFFSET mResultSum ;Load EDX with the mResultSum string OFFSET
+	call WriteString ;Show the message on sceen
+	call WriteInt ;Show the element value on sceen
+	call Crlf ;Go to next line
+
+	mov eax,[array1+ebx] ;move the value of z into EBX
+	sub eax,ECX ;EAX = 22 - ECX = 52
+	mov edx,OFFSET mResultDiff ;Load EDX with the mResultSum string OFFSET
+	call WriteString ;Show the message on screen
+	call WriteInt ;Show the element value on sceen
+	call Crlf ;Go to next line
+
 	exit
 main ENDP
 END main
